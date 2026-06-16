914d801ac0e92bcf62d9ca15d47acc95eba212d5
EstebanMM13 <2001estebanmartinez@gmail.com>
2026-06-16 22:07:20 +0200

    Arreglar lo de V1

--- api-gateway/src/main/java/com/estebanmm13/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/estebanmm13/api_gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.estebanmm13.api_gateway.config;
 
+import org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
@@ -15,8 +16,11 @@ public class GatewayConfig {
     @Bean
     public RouterFunction<ServerResponse> authServiceRoute() {
         return GatewayRouterFunctions.route("auth-service")
-                .route(RequestPredicates.path("/api/auth/**"),
+                .route(RequestPredicates.path("/api/v1/auth/**")
+                                .or(RequestPredicates.path("/api/v1/users/**"))
+                                .or(RequestPredicates.path("/api/v1/admin/**")),
                         HandlerFunctions.http())
+                .filter(FilterFunctions.rewritePath("/api/v1/(?<segment>.*)", "/api/${segment}"))
                 .filter(LoadBalancerFilterFunctions.lb("auth-service"))
                 .build();
     }
@@ -24,10 +28,11 @@ public class GatewayConfig {
     @Bean
     public RouterFunction<ServerResponse> moviesServiceRoute() {
         return GatewayRouterFunctions.route("movies-service")
-                .route(RequestPredicates.path("/api/movies/**")
-                                .or(RequestPredicates.path("/api/genres/**"))
-                                .or(RequestPredicates.path("/api/reviews/**")),
+                .route(RequestPredicates.path("/api/v1/movies/**")
+                                .or(RequestPredicates.path("/api/v1/genres/**"))
+                                .or(RequestPredicates.path("/api/v1/reviews/**")),
                         HandlerFunctions.http())
+                .filter(FilterFunctions.rewritePath("/api/v1/(?<segment>.*)", "/api/${segment}"))
                 .filter(LoadBalancerFilterFunctions.lb("movies-service"))
                 .build();
     }
